7d7bfa179109b1194e0028ac785d4836fd33ac63
Jeffrey Kirchner <jkirchner@gmail.com>
2022-10-31 15:35:14 -0700

    update startup

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 echo "*** Startup.sh ***"
 echo "Run Migrations:"
 python manage.py migrate
-echo "Install htop:"
-apt-get -y install htop
+# echo "Install htop:"
+# apt-get -y install htop
 echo "Start Daphne:"
-daphne -b 0.0.0.0 _distributed_consensus:application
+daphne -b 0.0.0.0 _distributed_consensus.asgi:application
