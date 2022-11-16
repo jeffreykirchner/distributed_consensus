ee78559c0cbc56fb92c54634dc8e65f804596528
Jeffrey Kirchner <jkirchner@gmail.com>
2022-11-16 11:07:56 -0800

    add sorting to session list page.

--- main/templates/staff/staff_home/sessions_card.js
+++ main/templates/staff/staff_home/sessions_card.js
@@ -23,3 +23,34 @@ sendDeleteSession(id){
     this.working = true;
     app.sendMessage("delete_session",{"id" : id});
 },
+
+/**
+ * sort by title
+ */
+ sort_by_title:function(){
+
+    app.working = true;
+
+    app.sessions.sort(function(a, b) {
+        a=a.title.trim().toLowerCase();
+        b=b.title.trim().toLowerCase();
+        return a < b ? -1 : a > b ? 1 : 0;
+    });
+
+    app.working = false;
+},
+
+/**
+ * sort by date
+ */
+sort_by_date:function(){
+
+    app.working = true;
+
+    app.sessions.sort(function(a, b) {
+        return new Date(b.start_date) - new Date(a.start_date);
+
+    });
+
+    app.working = false;
+},
