9e8850ec17565d92f08c45cb842801da72f898fe
Jeffrey Kirchner <jkirchner@gmail.com>
2023-10-25 09:46:06 -0700

    update to redis to local install.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,12 @@
 echo "*** Startup.sh ***"
 echo "Run Migrations:"
 python manage.py migrate
-# echo "Install htop:"
-# apt-get -y install htop
+echo "Install htop:"
+apt-get -y install htop
+echo "Install redis"
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+apt-get update
+apt-get -y install redis
 echo "Start Daphne:"
-daphne -b 0.0.0.0 _distributed_consensus.asgi:application
+redis-server & daphne -b 0.0.0.0 _distributed_consensus.asgi:application
